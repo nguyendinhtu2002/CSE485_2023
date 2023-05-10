595e5ec2b335dee881231663a505fcf7f01dfd81
TranVanTruong205 <trantruong260501@gmail.com>
2023-05-10 12:33:39 -0600

    upcode sua len

--- BTTH01/view/update-student.php
+++ BTTH01/view/update-student.php
@@ -11,14 +11,14 @@ if ($id) {
     $data = $studentDAO->getById($id);
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["name"]) && isset($_POST["age"]) && isset($_POST["grade"])) {
-    $id = $_POST["id"];
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["name"]) && isset($_POST["age"])) {
     $name = $_POST["name"];
     $age = $_POST["age"];
-    $grade = $_POST["grade"];
+    // $grade = $_POST["grade"];
 
     // $student = new Student($name, $age, $grade, $id);
-    $result = $studentDAO->updateStudentGrade($id, $grade);
+    $result = $studentDAO->updateStudentGrade($id,$age);
+    echo $result;
     if ($result) {
         header("Location: index.php");
         exit();
@@ -46,19 +46,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["name"]) && isset($_POS
 <body>
 
     <body>
-        <h1 style="text-align: center;color: blue;">Student Edit</h1>
-
-        <h2 style="color: blue;">Edit</h2>
-        <form action="update-student.php" method="POST">
+    <style>
+        #save-button,#cancel-button{
+            background-color: rebeccapurple;
+            margin-left: 1rem;
+            color:antiquewhite;
+            font-size: 1.5rem;
+            
+        }
+        #name,#age{
+            font-size: 1.5rem;
+            margin-right: 1.5rem;
+        }
+        body {
+			background-color: bisque;
+		}
+        </style>
+        <h1 style="text-align: center;color: blue;margin: 4rem 4rem">Student Edit</h1>
+        <form action="update-student.php" method="POST" style="text-align:center">
             <input type="hidden" name="id" value="<?php echo $data->getId(); ?>">
-            <label>Name:</label>
-            <input type="text" name="name" value="<?php echo $data->getName(); ?>">
-            <label>Age:</label>
-            <input type="text" name="age" value="<?php echo $data->getAge(); ?>">
-            <label>Grade:</label>
-            <input type="text" name="grade" value="<?php echo $data->getGrade(); ?>">
-            <input type="submit" value="Save">
-            <a href="list-student.php">Cancel</a>
+            <label style="font-size:24px;margin-right:1rem">Name:</label>
+            <input type="text" name="name" value="<?php echo $data->getName(); ?>"id='name'>
+            <label style="font-size:24px;margin-right:1rem">Age:</label>
+            <input type="text" name="age" value="<?php echo $data->getAge(); ?>" id='age' >
+            <button class="button" id="save-button" type="submit">Save</button>
+            <button class="button" id="cancel-button" type="button"><a href='index.php' style="text-decoration:none;color:aliceblue">Cancel</a></button>
         </form>
 
     </body>
