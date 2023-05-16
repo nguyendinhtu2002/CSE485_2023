c90eaf76de09e5c0c2f8f8765832e1d116f4441d
nguyendinhtu2002 <nguyendinhtu11022002@gmail.com>
2023-05-16 21:44:04 +0700

    update

--- BTTH02/controller/StudentController.php
+++ BTTH02/controller/StudentController.php
@@ -1,5 +1,19 @@
 <?php
 
-class StudenController{
-    
+require_once 'C:\xampp\htdocs\CSE485_2023\BTTH02\model\Connect.php';
+
+class StudenController
+{
+
+
+    public function getALL()
+    {
+        $conect = new Connect();
+
+        $stmt = $conect->query('SELECT * FROM students');
+        $students = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        echo $students;
+
+    }
 }
---
+++ BTTH02/view/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'C:\xampp\htdocs\CSE485_2023\BTTH02\controller\StudentController.php';
+
+$data = new StudenController();
+
+$data->getALL();
